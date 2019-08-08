e0f550d70ad54ac5fd0364f7771d691642aad429
MagicalBrain <13542593649@163.com>
2019-08-08 21:33:08 +0800

    修改了一下项目文件结构

--- 顺序表_research/TestForFuntion.h
+++ 顺序表_research/TestForFuntion.h
@@ -5,9 +5,15 @@
 
 using namespace std;
 
+/*
+TestFunction.h:
+只放辅助测试函数
+即用来测试的另外的一些操作，包括生成随机有序序列什么的
+*/
+
 //const int len = 80;
 
-//生成随机数列函数
+//生成随机无序数列函数
 int* CreatRand()
 /*
 输入数字n，生成n个随机元素的数组
@@ -35,19 +41,100 @@ int* CreatRand()
 	return p;
 }
 
-//创建线性表函数
-void CreatList(SqList* L)
+//生成随机有序数列函数
+int* CreatRand1()
 /*
-
+输入数字n，生成n个随机元素的数组
 */
 {
-	int* p;
-	p = CreatRand();
+	int* p,tmp;
+	cout << "输入随机数的个数" << endl;
+	int n, len;
+	cin >> n;
+	cout << "输入随机数的大小范围：" << endl;
+	cin >> len;
+
+	p = (int*)malloc((n + 1) * sizeof(int));
+	p[0] = n;
+
+	cout << endl << "生成的随机数：" << endl;
+	srand((unsigned int)time(0));
+	for (int i = 1; i < n + 1; i++)
+	{
+		int re = rand();
+		p[i] = re % len;
+		cout << p[i] << " ";
+	}
+	cout << endl;
+
+	for (int j=1;j<p[0]+1;j++)
+	{
+		for (int i = 1; i < p[0]; i++)
+		{
+			if (p[i] > p[i+1])
+			{
+				tmp = p[i];
+				p[i] = p[i+1];
+				p[i+1] = tmp;
+			}
+		}
+	}
+	
+	for (int i = 1; i <= p[0]; i++)
+	{
+		cout << p[i] << " ";
+	}
+	cout << endl;
+	return p;
+}
+
+//创建线性表函数
+void CreatList(SqList* L,int x)
+/*
+L:处理的线性表
+x:1->有序；0->无序
+*/
+{
+	int* p = NULL;
+	switch (x)
+	{
+	case 1:
+		p = CreatRand1();
+		break;
+	case 0:
+		p = CreatRand();
+		break;
+	default:
+		p = NULL;
+		break;
+	}
+	//p = CreatRand();
 
 	int n = p[0];
 	for (int i = 0; i < n; i++)
 	{
 		ListInsert(L, p[i+1], i);
+	}
+}
+
+void CreatList(SqList* L1,SqList* L2)
+/*
+L:处理的线性表
+x:1->有序；0->无序
+*/
+{
+	int* p1,* p2;
+	p1 = CreatRand();
+	p2 = CreatRand();
+
+	int n1 = p1[0],n2=p2[0];
+	for (int i = 0; i < n1; i++)
+	{
+		ListInsert(L1, p1[i + 1], i);
+	}
+	for (int i = 0; i < n2; i++)
+	{
+		ListInsert(L2, p2[i + 1], i);
 	}
 }
 
--- 顺序表_research/main.cpp
+++ 顺序表_research/main.cpp
@@ -1,275 +1,13 @@
 #include <iostream>
 #include "List.h"
 #include "TestForFuntion.h"
+#include "func.h"
 
 using namespace std;
 
-int func01(SqList* L)
 /*
-王道书 P18 第二章第二节 综合题1
-*/
-{
-	int min,loca=0,l=Length(L);
-
-	if (l == 0)
-	{
-		cout << "该线性表为空，无法删除！（fun01）" << endl;
-		return 0;
-	}
-	else
-	{
-		min = L->elem[0];
-
-		for (int i = 0; i < l; i++)
-		{
-			if (L->elem[i] < min)
-			{
-				min = L->elem[i];
-				loca = i;
-			}
-		}
-
-		ListDelete(L, loca);
-		ListInsert(L, L->elem[l - 1], loca);
-		L->length--;
-		return 1;
-		//cout<<""
-	}
-}
-
-int func02(SqList* L)
-/*
-王道书 P18 第二章第二节 综合题2
-*/
-{
-	int t;
-	if (Length(L) < 1)
-	{
-		cout << "线性表为空！" << endl;
-		return 0;
-	}
-	else
-	{
-		for (int i = 0; i < (Length(L) /2); i++)
-		{
-			t = L->elem[i];
-			L->elem[i] = L->elem[Length(L) - 1 - i];
-			L->elem[Length(L) - 1 - i] = t;
-		}
-		return 1;
-	}
-		
-}
-
-int func03(SqList* L)
-/*
-王道书 P18 第二章第二节 综合题3
-*/
-{
-	//int l = Length(L), t;
-	int e;
-	cout << "输入要删除的键值：" << endl;
-	cin >> e;
-	if (Length(L) < 1)
-	{
-		cout << "线性表为空！" << endl;
-		return 0;
-	}
-	else
-	{
-		for (int i = 0; i < Length(L); i++)
-		{
-			if (L->elem[i] == e)
-				ListDelete(L, i);
-		}
-		return 1;
-	}
-}
-
-int func04(SqList* L)
-/*
-王道书 P18 第二章第二节 综合题4
-*/
-{
-	int s, t,l=Length(L),p;
-	cout << "输入要删除的元素值的范围：" << endl;
-	cin >> s >> t;
-
-	if (l < 1)
-	{
-		cout<<"线性表长度不合理！"<<endl;
-		return 0;
-	}
-	else if (s >= t)
-	{
-		cout << "输入数值范围不合理！" << endl;
-		return 0;
-	}
-	else
-	{
-		for (int i = 0; i < l; i++)
-		{
-			if (L->elem[i] > s && L->elem[i] < t)
-			{
-				p = L->elem[i + 1];
-				int n = 0;
-				while (p > s && p < t)
-				{
-					ListDelete(L, i + 1 + n);
-					n++;
-					p = L->elem[i + 1 + n];
-				}
-				ListDelete(L, i);
-			}
-		}
-	}
-	return 1;
-}
-
-int func05(SqList* L)
-/*
-王道书 P18 第二章第二节 综合题5
-*/
-{
-	int s, t, l = Length(L), p;
-	cout << "输入要删除的元素值的范围：" << endl;
-	cin >> s >> t;
-
-	if (l < 1)
-	{
-		cout << "线性表长度不合理！" << endl;
-		return 0;
-	}
-	else if (s >= t)
-	{
-		cout << "输入数值范围不合理！" << endl;
-		return 0;
-	}
-	else
-	{
-		for (int i = 0; i < l; i++)
-		{
-			if (L->elem[i] >= s && L->elem[i] <= t)
-			{
-				p = L->elem[i + 1];
-				int n = 0;
-				while (p >= s && p <= t)
-				{
-					ListDelete(L, i + 1 + n);
-					n++;
-					p = L->elem[i + 1 + n];
-				}
-				ListDelete(L, i);
-			}
-		}
-	}
-	return 1;
-}
-
-int func06(SqList* L)
-{
-	int l = Length(L),num=0,flag=0;
-	ElemType* p;
-
-	if (l < 1)
-	{
-		cout << "线性表长度不合理" << endl;
-		return 0;
-	}
-	else
-	{
-		p = (ElemType*)malloc(l * sizeof(ElemType));
-		p[0] = L->elem[0];
-		num = 1;
-
-
-		for (int i = 1; i < l; i++)
-		{
-			for (int j = 0; j < num; j++)
-			{
-				if (L->elem[i] != p[j])
-				{
-					flag++;
-				}
-			}
-			if (flag == num)
-			{
-				p[num] = L->elem[i];
-				num++;
-			}
-			flag = 0;
-		}
-		
-
-		for (int i = 0; i < num; i++)
-		{
-			L->elem[i] = p[i];
-		}
-		L->length = num;
-	}
-	return 1;
-}
-
-//有序顺序表求交集的函数 (该函数存在问题，输出结果不对！)
-/*
-SqList Jiao(SqList n1,SqList n2)
-{
-	SqList* re;
-	ElemType* w;
-	int n;
-	if (n1.length > n2.length)
-		n = n2.length;
-	else
-		n = n1.length;
-
-	int k = 0;
-	w = (ElemType*)malloc(n * sizeof(ElemType));
-
-	re = (SqList*)malloc(sizeof(SqList));
-	re->elem = w;
-	//re->size = n;
-
-	for (int i = 0; i < n1.length; i++)
-	{
-		for (int j = 0; j < n2.length; j++)
-		{
-			if (n1.elem[i] == n2.elem[j])
-			{
-				w[k] = n1.elem[i];
-				k++;
-				//re->length++;
-			}
-
-		}
-	}
-
-	re->length = k;
-
-	return *re;
-}
-
-void test(int n1[],int n2[])
-{
-	//cout << sizeof(n1) / sizeof(int) << endl;
-	SqList* L1, * L2;
-	L1 = (SqList*)malloc(sizeof(SqList));
-	L2 = (SqList*)malloc(sizeof(SqList));
-
-	input(*L1, n1, sizeof(n1) / sizeof(int));
-	input(*L2, n2, sizeof(n2) / sizeof(int));
-
-
-
-	output(*L1);
-	output(*L2);
-
-	//Inv(*L);
-	SqList re = Jiao(*L1, *L2);
-	output(re);
-}
-
-int n1[] = {1,2,3,4,5,6,8,10,11};
-int n2[] = { 1,2,3,4,5,7,9,10,11,12,13 };
+main.cpp
+只放测试函数和主函数
 */
 
 int test()
@@ -283,14 +21,19 @@ int test()
 	InitList(L);
 	InitList(L1);
 
-	int l = Length(L);
-	cout << l << endl;
+	
 
-	CreatList(L);
-	cout << L->length << endl;
+	//CreatList(L,L1);
+	CreatList(L,1);
 
+	cout << endl;
+	//CreatRand1();
 	PrintList(L);
+	//PrintList(L1);
 	//创建线性表并打印，结束
+
+	//int l = Length(L);
+	//cout << l << endl;
 
 	//test_LocateElem(L);
 	//test_GetElem(L);
@@ -304,6 +47,7 @@ int test()
 	func06(L);
 
 	PrintList(L);
+	//PrintList(L1);
 	//DestroyList(L);
 
 	return 1;
